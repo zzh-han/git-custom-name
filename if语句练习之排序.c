cfa06706c238c2ded137bc6ccbb20cdf686121b5
zzh-han <3126220240@qq.com>
2026-06-27 22:06:43 +0800

    init commit

---
+++ if语句练习之排序.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+
+int main(){
+    int a , b , c , x;
+    printf("please input : \n");
+    scanf("%d%d%d" , &a , &b , &c);
+        if (b <= a){
+            x = a ;
+            a = b ;
+            b = x ;
+         }
+         if (c <= a){
+            x = a ;
+            a = c ;
+            c = x ;
+         }
+         if (c <= b){
+            x = b ;
+            b = c ;
+            c = x ;
+         }
+    printf("%d %d %d\n" , a , b , c);
+    return 0;
+}
+
